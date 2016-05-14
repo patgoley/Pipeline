398a6f7e5a4da033028c515bafebc9115740e3fe
Patrick Goley <patrick.goley@gmail.com>
2016-05-14 14:06:15 -0500

    Initial Commit

---
+++ Pipeline/Pipeline.h
@@ -0,0 +1,19 @@
+//
+//  Pipeline.h
+//  Pipeline
+//
+//  Created by Patrick Goley on 5/14/16.
+//  Copyright © 2016 arbiter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pipeline.
+FOUNDATION_EXPORT double PipelineVersionNumber;
+
+//! Project version string for Pipeline.
+FOUNDATION_EXPORT const unsigned char PipelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pipeline/PublicHeader.h>
+
+
