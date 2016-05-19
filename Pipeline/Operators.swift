57e8606438d319ca4607e77483a059f42cc858b0
Patrick Goley <patrick.goley@gmail.com>
2016-05-19 15:50:46 -0500

    operators for consumable pipelines

--- Pipeline/Operators.swift
+++ Pipeline/Operators.swift
@@ -11,14 +11,33 @@ import Foundation
 
 infix operator |>   { precedence 50 associativity left }
 
-public func |> <T: TransformerType, U: TransformerType where T.OutputType == U.InputType>(lhs: T, rhs: U) -> TransformerPipeline<T.InputType, U.OutputType>  {
+// MARK: - Consumable
+
+public func |> <S: ConsumableType, T: TransformerType where S.OutputType == T.InputType>(lhs: S, rhs: T) -> ConsumablePipeline<T.OutputType>  {
     
-    return TransformerPipeline(head: lhs).then(rhs)
+    return ConsumablePipeline(head: lhs).then(rhs)
 }
+
+public func |> <S: ConsumableType, NewOutput>(lhs: S, rhs: S.OutputType -> NewOutput) -> ConsumablePipeline<NewOutput>  {
+    
+    return ConsumablePipeline(head: lhs).then(rhs)
+}
+
+// MARK: - ProducerPipeline
 
 public func |> <P: ProducerType, U: TransformerType where P.OutputType == U.InputType>(lhs: P, rhs: U) -> ProducerPipeline<U.OutputType>  {
     
     return ProducerPipeline(head: lhs).then(rhs)
+}
+
+public func |> <P: ProducerType, U>(lhs: P, rhs: P.OutputType -> U) -> ProducerPipeline<U>  {
+    
+    return ProducerPipeline(head: lhs).then(rhs)
+}
+
+public func |> <O, C where C: TransformerType, O == C.InputType>(lhs: ProducerPipeline<O>, rhs: C) -> ProducerPipeline<C.OutputType>  {
+    
+    return lhs.then(rhs)
 }
 
 public func |> <V, T: TransformerType where V == T.InputType>(lhs: V, rhs: T) -> ProducerPipeline<T.OutputType>  {
@@ -35,31 +54,6 @@ public func |> <V, T: TransformerType where V == T.InputType>(lhs: () -> V, rhs:
     return ProducerPipeline(head: thunkProducer).then(rhs)
 }
 
-public func |> <T: TransformerType, U>(lhs: T, rhs: T.OutputType -> U) -> TransformerPipeline<T.InputType, U>  {
-    
-    return TransformerPipeline(head: lhs).then(rhs)
-}
-
-public func |> <P: ProducerType, U>(lhs: P, rhs: P.OutputType -> U) -> ProducerPipeline<U>  {
-    
-    return ProducerPipeline(head: lhs).then(rhs)
-}
-
-public func |> <I, O, U where U: TransformerType, O == U.InputType>(lhs: TransformerPipeline<I, O>, rhs: U) -> TransformerPipeline<I, U.OutputType>  {
-    
-    return lhs.then(rhs)
-}
-
-public func |> <O, C where C: TransformerType, O == C.InputType>(lhs: ProducerPipeline<O>, rhs: C) -> ProducerPipeline<C.OutputType>  {
-    
-    return lhs.then(rhs)
-}
-
-public func |> <I, O, C>(lhs: TransformerPipeline<I, O>, rhs: O -> C) -> TransformerPipeline<I, C>  {
-    
-    return lhs.then(rhs)
-}
-
 public func |> <O, C>(lhs: ProducerPipeline<O>, rhs: O -> C) -> ProducerPipeline<C>  {
     
     return lhs.then(rhs)
@@ -73,6 +67,28 @@ public func |> <O, C: ConsumerType where C.InputType == O>(lhs: ProducerPipeline
 public func |> <O>(lhs: ProducerPipeline<O>, rhs: O -> Void) -> ProducerPipeline<O>  {
     
     return lhs.finally(rhs)
+}
+
+// MARK: - TransformerPipeline
+
+public func |> <T: TransformerType, U: TransformerType where T.OutputType == U.InputType>(lhs: T, rhs: U) -> TransformerPipeline<T.InputType, U.OutputType>  {
+    
+    return TransformerPipeline(head: lhs).then(rhs)
+}
+
+public func |> <T: TransformerType, U>(lhs: T, rhs: T.OutputType -> U) -> TransformerPipeline<T.InputType, U>  {
+    
+    return TransformerPipeline(head: lhs).then(rhs)
+}
+
+public func |> <I, O, U where U: TransformerType, O == U.InputType>(lhs: TransformerPipeline<I, O>, rhs: U) -> TransformerPipeline<I, U.OutputType>  {
+    
+    return lhs.then(rhs)
+}
+
+public func |> <I, O, C>(lhs: TransformerPipeline<I, O>, rhs: O -> C) -> TransformerPipeline<I, C>  {
+    
+    return lhs.then(rhs)
 }
 
 public func |> <I, O, C: ConsumerType where C.InputType == O>(lhs: TransformerPipeline<I, O>, rhs: C) -> TransformerPipeline<I, O>  {
