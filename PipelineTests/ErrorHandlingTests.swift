86db18e5e43236047a63e1ca027ee3998c948486
Patrick Goley <patrick.goley@gmail.com>
2016-05-31 00:14:31 -0500

    complete test coverage

--- PipelineTests/ErrorHandlingTests.swift
+++ PipelineTests/ErrorHandlingTests.swift
@@ -19,5 +19,49 @@ class ErrorHandlingTests: XCTestCase {
         
         pipe.produce()
     }
-
+    
+    func testForceUnwrap() {
+        
+        let string: String? = "123"
+        
+        let pipe = ValueProducer(string) |> forceUnwrap |> { (x: String) in XCTAssert(x == "123") }
+        
+        pipe.produce()
+    }
+    
+    func testForceUnwrapClosure() {
+        
+        let string: String? = "123"
+        
+        let pipe = ValueProducer(string) |> forceUnwrap { (str: String?) in str?.characters.count } |> { (x: Int) in x == 3  }
+        
+        pipe.produce()
+    }
+    
+    func testDownCast() {
+        
+        let string = "123"
+        
+        let pipe = ValueProducer(string) |> downCast(Int.self) |> { _ in XCTAssert(false) }
+        
+        pipe.produce()
+    }
+    
+    func testForceCast() {
+        
+        let anyObject: AnyObject = NSNumber(int: 3)
+        
+        let pipe = ValueProducer(anyObject) |> forceCast(NSNumber.self) |> { x in XCTAssert(x.intValue == 3) }
+        
+        pipe.produce()
+    }
+    
+    func testSwallowError() {
+        
+        let result: Result<String> = .Error(NSError(domain: "", code: 0, userInfo: nil))
+        
+        let pipe = ValueProducer(result) |> swallowError() |> { _ in XCTAssert(false) }
+        
+        pipe.produce()
+    }
 }
---
+++ PipelineTests/TransformerTests.swift
@@ -0,0 +1,46 @@
+//
+//  TransformerTests.swift
+//  Pipeline
+//
+//  Created by Patrick Goley on 5/24/16.
+//  Copyright © 2016 arbiter. All rights reserved.
+//
+
+import XCTest
+import Pipeline
+
+class TransformerTests: XCTestCase {
+    
+    func testPassThroughTransformer() {
+        
+        let pipe = ValueProducer("123")
+            |> PassThroughTransformer() { (x: String) in XCTAssert(x == "123") }
+            |> { (x: String) in XCTAssert(x == "123") }
+        
+        pipe.produce()
+    }
+    
+    func testAnyTransformerNoConsumer() {
+        
+        let transformer = AnyTransformer<String, String>() { (x: String) in
+            
+            XCTAssert(false)
+            
+            return x + "4"
+        }
+        
+        transformer.consume("123")
+    }
+    
+    func testAsyncTransformerNoConsumer() {
+        
+        let transformer = AsyncTransformer() { (x: String, consumer: String -> Void) in
+            
+            XCTAssert(false)
+            
+            consumer(x + "4")
+        }
+        
+        transformer.consume("123")
+    }
+}
